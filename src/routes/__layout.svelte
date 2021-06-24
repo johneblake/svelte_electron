83cf16b7fe8f06c9694d2741fdb764d214e6877d
John <john@gellert.com>
2021-06-24 13:32:26 -0600

    More cleanup

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	import '../app.postcss';
-	import { onMount } from 'svelte';
+  import '../app.postcss';
+  import { onMount } from 'svelte';
 
-	let ready: boolean = false;
-	onMount(() => (ready = true));
+  let ready: boolean = false;
+  onMount(() => (ready = true));
 </script>
 
 <div class="dragbar" />
 
 {#if ready}
-	<slot />
+  <slot />
 {/if}
 
 <style>
-	.dragbar {
-		-webkit-app-region: drag;
-		position: absolute;
-		z-index: 100;
-		height: 40px;
-		width: 100%;
-	}
+  .dragbar {
+    -webkit-app-region: drag;
+    position: absolute;
+    z-index: 100;
+    height: 40px;
+    width: 100%;
+  }
 </style>
